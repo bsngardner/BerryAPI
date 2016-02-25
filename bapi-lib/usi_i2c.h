99650d49b2280250e803662af4c6e120dd7b1476
Kristian Sims <kbsims@byu.edu>
2016-02-24 18:29:05 -0700

    Added bapi-lib as a library, and bapi_genII-accel refers to it.

---
+++ bapi-lib/usi_i2c.h
@@ -0,0 +1,27 @@
+/*
+ * usi_i2c.h
+ *
+ *  Created on: Nov 5, 2015
+ *      Author: Broderick
+ */
+
+#ifndef USI_I2C_H_
+#define USI_I2C_H_
+
+#include <stdint.h>
+
+//Defines
+#define SDA_PIN BIT7
+#define SCL_PIN BIT6
+
+//Prototypes
+void usi_init();
+__interrupt void USI_TXRX(void);
+
+//Extern functions
+extern void set_register(uint8_t value);
+extern uint8_t get_register();
+
+//Extern variables
+
+#endif /* USI_I2C_H_ */
