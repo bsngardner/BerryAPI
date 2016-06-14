12058491d50437ed9938f024921847bb82f1558b
Broderick Gardner <bsn.gardner@gmail.com>
2016-06-14 15:20:55 -0600

    Added b09-pot project

---
+++ berry09-pot/adc.c
@@ -0,0 +1,19 @@
+/*
+ * adc.c
+ *
+ *  Created on: Jun 14, 2016
+ *      Author: Broderick
+ */
+
+#include <msp430.h>
+#include "adc.h"
+
+#define ADC10_CTL	(SREF_0 \ //Vcc and GND
+					| 
+
+)
+
+//init the 10 bit adc
+void adc_init(){
+	
+}
---
+++ berry09-pot/adc.h
@@ -0,0 +1,10 @@
+/*
+ * adc.h
+ *
+ *  Created on: Jun 14, 2016
+ *      Author: Broderick
+ */
+
+#define ADC_IN
+
+void adc_init();
