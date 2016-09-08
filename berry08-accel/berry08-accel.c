6ece90ba2b3fd6657fc6184a0696f4e326128330
Broderick Gardner <bsn.gardner@gmail.com>
2016-09-07 18:30:48 -0600

    working servo, added accelerometer project

---
+++ berry08-accel/berry08-accel.c
@@ -0,0 +1,73 @@
+/*
+ * Firmware for beeper device, berry28-beep
+ * 		Broderick Gardner
+ * 		6/15/2016
+ */
+
+#include <msp430.h> 
+
+#include "berry.h"
+
+#define DEV_TYPE 0x08
+
+const int led0_port = 1;
+const int led0_pin = BIT0;
+
+#define R_READ0 2
+#define R_READ1 3
+#define R_READB 4
+
+volatile uint16_t level = 4;
+
+void timer_init();
+
+uint8_t device_init()
+{
+
+	return DEV_TYPE;
+}
+
+void tick()
+{
+
+}
+
+void set_register(uint8_t value)
+{
+	switch (current_register)
+	{
+	case 2:
+		registers[2] = value;
+
+		break;
+	case 3:
+
+		break;
+	case 4:
+
+		break;
+	}
+
+	return;
+}
+
+uint8_t get_register()
+{
+	switch (current_register)
+	{
+	//On reading each byte of the potentiometer value,
+	//	switch current register to the other byte
+	//	This allows reading 2 bytes in quick succession
+	case R_READ0:
+		current_register = R_READ1;
+		return registers[R_READ0];
+	case R_READ1:
+		current_register = R_READ0;
+		return registers[R_READ1];
+	case R_READB:
+		return registers[R_READB];
+	default:
+		break;
+	}
+	return 0;
+}
--- berry29-servo/berry29-servo.c
+++ berry29-servo/berry29-servo.c
@@ -24,11 +24,6 @@ const int led0_pin = BIT1;
 #define R_READ1 3
 #define R_READB 4
 
-#define SET_PWM		TA0CCTL1 |= OUTMOD_7
-#define SET_ONOFF	TA0CCTL1 &= ~OUTMOD_7
-#define SET_ON		TA0CCTL1 |= OUT
-#define SET_OFF		TA0CCTL1 &= ~OUT
-
 void timer_init();
 
 uint8_t device_init()
@@ -39,10 +34,13 @@ uint8_t device_init()
 	P1SEL0 |= MOTOR_PIN;
 
 	timer_init();
-	SET_OFF;
 
 	registers[2] = 0;	//default config to on/off
 	registers[3] = 0;	//start off
+
+	current_register = 2;
+	set_register(150);
+
 	return DEV_TYPE;
 }
 
@@ -51,31 +49,27 @@ void tick()
 
 }
 
+//Map 8 bit register to (1428,7572), which is about .5ms to 2.5ms
+//	true .5ms to 2.5ms is (1500,7500) given a 3MHz clock and 16bit timer
+#define OFFSET 2856 //1500 (true .5ms) minus 72 (to round scale factor to 6144)
+
 void set_register(uint8_t value)
 {
+	uint16_t duty;
 	switch (current_register)
 	{
 	case 2:
 		registers[2] = value;
-		if (value)
-		{
-			SET_ONOFF;
-		}
-		else
-		{
-			SET_PWM;
-		}
+		duty = (value << 6) + (value << 5);
+		duty = (duty + 1) >> 1;
+		duty += OFFSET;
+		TA0CCR2 = duty;
 		break;
 	case 3:
 		registers[3] = value;
-		if (value)
-			SET_ON;
-		else
-			SET_OFF;
 		break;
 	case 4:
 		registers[4] = value;
-		TA1CCR1 = value;
 		break;
 	}
 
@@ -105,9 +99,8 @@ uint8_t get_register()
 
 void timer_init()
 {
-	TA0CCTL0 = 0; // TA1CCR0 interrupt enabled
-	TA0CCTL1 = OUTMOD_0; // TA1CCR0 interrupt enabled
-	TA0CTL = TACLR | TASSEL_1 | MC_1; // ACLK, continuous mode
-	TA0CCR0 = 0xff;
-	TA0CCR1 = 0xff >> 1;
+	TA0CCTL2 = OUTMOD_7;
+	TA0CCR0 = 1;
+	TA0CCR2 = 0;
+	TA0CTL = TACLR | TASSEL_2 | ID_2 | MC_2; // SMCLK, continuous mode
 }
